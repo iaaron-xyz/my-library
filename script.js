fed1ba8b0580cc12244bd2f81b52765657c1704f
Aaron <iaaron.xyz@outlook.com>
2023-01-07 02:47:45 -0600

    implement toggle read status function

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ let readStatus = ""
 let firstLoad = true;
 
 const boxCards = document.getElementsByClassName("box-card");
+const readStatusBtn = document.getElementsByClassName("read-status-btn");
 const newBookBtn = document.getElementById("new-book-btn");
 const addBookInfoBtn = document.getElementById("add-book-info-btn");
 const booksSection = document.getElementById("books-section");
 const modalClose = document.getElementById("modal-close");
 const modalForm = document.getElementById("modal-form");
+
 
 /**
  * MAIN DOM MANIPULATION
@@ -42,6 +44,7 @@ createBookCards();
 // Interactive events
 newBookBtn.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
+
 
 /**
  * FUNCTIONS
@@ -70,11 +73,11 @@ function createBookCards() {
   for (let i = 0; i < myLibrary.length; i++) {
     // Read status options
     if (myLibrary[i].read) {
-      readStatus = `<button type="button" class="button is-success">Read</button>`
+      readStatus = `<button type="button" onclick="toggleReadStatus(event)" class="button is-success read-status-btn" id=${i}>Read</button>`;
     }
     else {
-      readStatus = `<button type="button" class="button is-black">
-      Not Read</button>`
+      readStatus = `<button type="button" onclick="toggleReadStatus(event)" class="button read-status-btn" id="${i}">
+      Not Read</button>`;
     }
     // Book Card HTML
     booksSection.innerHTML += `<div class="box box-card" id="${i}">
@@ -111,6 +114,22 @@ function deleteBookCard(e) {
   }
   // Re-create the book cards with the new list
   createBookCards();
+}
+
+// Toggle read status when read/not-read clicked
+function toggleReadStatus(e) {
+  // toggle class is-success
+  e.target.classList.toggle("is-success");
+  // Change button content and the list object
+  if (Array.from(e.target.classList).includes("is-success")) {
+    e.target.innerHTML = "Read";
+    myLibrary[e.target.id].read = true;
+  }
+  else {
+    e.target.innerHTML = "Not read";
+    myLibrary[e.target.id].read = false;
+  }
+  console.log(myLibrary[e.target.id])
 }
 
 // Functions to open and close a modal
